ef361f2db0bf16ab65c498551a5a6621f44be407
przemekk1385 <przemek13@gmail.com>
2017-12-10 19:48:45 +0100

    config update

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
+import os
+
+
 class Config(object):
     """
     Common configuration
     """
 
     BABEL_DEFAULT_LOCALE = 'pl'
+    BOARD_ID = 2
     ITEMS_PER_PAGE = 20
     MAX_FILENAME_LENGTH = 100
+    MANAGEMENT_ID = 3
+    MEDIA_DIR = os.path.join(os.getcwd(), 'media')
+    UPLOAD_DIR = os.path.join(os.getcwd(), 'upload')
 
 
 class Development(Config):
